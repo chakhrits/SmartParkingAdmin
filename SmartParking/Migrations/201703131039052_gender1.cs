56fd864688ce7df41386287b244f2b6c9220b73b
Chakhrit_017 <chakhritz@gmail.com>
2017-03-14 10:48:07 +0700

    EasyParkingAdmin
    EasyParkingAdmin

---
+++ SmartParking/Migrations/201703131039052_gender1.cs
@@ -0,0 +1,16 @@
+namespace SmartParking.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class gender1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
